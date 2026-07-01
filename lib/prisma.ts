9362d9413f47572d6c1b50b5a86bc63849b944f0
josthan12 <josthan2004wong@gmail.com>
2026-07-01 11:17:25 +0800

    Fix: commit generated prisma client, remove from gitignore

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@/app/generated/prisma"
+import { PrismaPg } from "@prisma/adapter-pg"
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
